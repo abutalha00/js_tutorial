749d37278162d6c038989fd0281b9685946e80fe
abutalha00 <75663808+abutalha00@users.noreply.github.com>
2023-10-31 18:22:56 +0000

    Variables, datatypes and datatype conersion

---
+++ variables/test.js
@@ -0,0 +1,27 @@
+"use strict" // run my code in newer JS syntx
+
+const constant = "HELLO"
+let userId = 3987483
+
+/* Datatypes in js
+
+Premetive Datatype
+
+string = "this is string value"
+number = 123456
+boolean = true/false
+bigInt 
+null => standalone value (type is object)
+undefined (undefined is a datatype and value too)
+
+
+Non-Premetive Datatypes
+Object
+
+*/
+
+console.table([constant, userId])
+console.log(
+    "Null datatype is object and it can assign as value in variable: " + typeof null,
+    typeof undefined
+)
